860af5b208e6d0180e11d35e2c411fa6d35230f0
jMabunda2025 <mabundajorge@gmail.com>
2026-03-07 01:08:33 +0200

    Modify payment.php for token and URL changes
    Updated payment processing with new token and base URL.

--- payment.php
+++ payment.php
@@ -3,15 +3,15 @@ require_once __DIR__ . '/XipatchiClient.php';
 
 use Xipatchi\XipatchiClient;
 
-$token = "696f53ffd50b4963f6496c4ac5faeb2a587bd975828e89964e668a641d95c410";
-$client = new XipatchiClient("http://localhost/xipatchi", $token);
+$token = "YOU_TOQUEN";
+$client = new XipatchiClient("URL_BASE", $token);
 
 $dados = [
     "cliente_id" => 2,
     "developer_id" => 1,
     "item_id"    => 1,
     "metodo"     => "mpesa",
-    "mpesa"      => "842252999",
+    "mpesa"      => "841234567",
     "amount"     => 500
 ];
 
@@ -19,4 +19,5 @@ $resultado = $client->processarPagamento($dados);
 
 header('Content-Type: application/json');
 echo json_encode($resultado, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
 
